dc267194081ae6025f58c931061d755bf79037db
jvdmosel <jvdmosel@gmail.com>
2022-06-26 20:03:01 +0200

    Add day 9

---
+++ 2015/9/1.py
@@ -0,0 +1,27 @@
+from itertools import permutations
+
+cities = set()
+E = {}
+
+with open("input", "r") as reader:
+    for line in reader:
+        a, _, b, _, distance = line.split(" ")
+        distance = int(distance.strip())
+        cities.add(a)
+        cities.add(b)
+        if a not in E.keys():
+            E[a] = {}
+        if b not in E.keys():
+            E[b] = {}
+        E[a][b] = distance
+        E[b][a] = distance
+
+# int max value
+shortest_route = 2 ** 63 - 1
+for perm in permutations(cities):
+    route = 0
+    for i in range(len(perm)-1):
+        route += E[perm[i]][perm[i+1]]
+    if route < shortest_route:
+        shortest_route = route
+print(shortest_route)
