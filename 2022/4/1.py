8acb2d08d08c37e05b10bf17f32927ea746b0104
jvdmosel <jvdmosel@gmail.com>
2022-12-04 17:23:18 +0100

    Add 2022 day 4

---
+++ 2022/4/1.py
@@ -0,0 +1,17 @@
+def fully_contained(A: int, B: int, C: int, D: int) -> bool:
+    return (A <= C and B >= D) or (A == C and B == D) or (A >= C and B <= D)
+
+
+with open("input", 'r') as reader:
+    sum = 0
+    for line in reader:
+        pair = line.split(",")
+        range_1 = pair[0].split("-")
+        range_2 = pair[1].strip().split("-")
+        A = int(range_1[0])
+        B = int(range_1[1])
+        C = int(range_2[0])
+        D = int(range_2[1])
+        if fully_contained(A, B, C, D):
+            sum += 1
+    print(sum)
