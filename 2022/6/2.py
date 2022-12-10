ccba9acc44a28a83edce7c0aad75e9bf76b62ad3
jvdmosel <jvdmosel@gmail.com>
2022-12-10 01:36:53 +0100

    Add 2022 day 6

---
+++ 2022/6/2.py
@@ -0,0 +1,21 @@
+def is_unique(s: str):
+    present = 0
+    for c in s:
+        # get correspnding bit
+        bit = ord(c)-ord("a")
+
+        # check if bit is already present
+        if present & (1 << bit) > 0:
+            return False
+
+        # set bit
+        present |= (1 << bit)
+    return True
+
+
+with open("input", 'r') as reader:
+    for line in reader:
+        for i in range(len(line)):
+            if is_unique(line[i:i+14]):
+                print(i+14)
+                break
